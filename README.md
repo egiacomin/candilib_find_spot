03782bfe5fc6f68ac7714b6825e1b5dc0f9bcd1a
edouard giacomin <edouardgiacomin@edouards-MacBook-Pro.local>
2020-08-12 14:42:54 -0600

    Update parameters

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ geckodriver (on macOS, can be installed using brew install geckodriver)
 
 **To run:**\
 1- Open search_spot.py and modify the variables as wanted (number, text, etc).\
-2- python3 search_spot.py\
+2- python3 search_spot.py phone_number (with extension) refresh_time (in s) County to look a place for (ex: 69)\
 3- Wait to receive an imessage!
--- search_spot.py
+++ search_spot.py
@@ -1,31 +1,69 @@
 from selenium import webdriver
 import time
-import urllib
 import os
 import re
+import argparse
 
-def main ():
-	#Define the different variables
-	page = "https://beta.interieur.gouv.fr/candilib/candidat/93/selection/selection-centre"
-	refresh_rate_seconds = 1000
-	tel = +1XXXXXXXX
-	text_to_send = "A spot is available in the Rhone-Aleps (69) county!"
+def search_places_text(county,string):
+	start_string = "          " + county
+	stop_string = "disponible"
+
+	f = string.splitlines()
+
+	for lnb, l in enumerate(f):
+		if start_string in l:
+			lnbcur = lnb + 1
+			while stop_string not in f[lnbcur-1]:
+				lnbcur = lnbcur + 1
+			#Get the number of places available
+			places = re.findall(r'\d+', f[lnbcur-1])
+			
+			if int(places[0]) > 0:
+				return True
+			else:
+				return False
+
+
+def scan_page(page,phone,refresh_time_seconds,county):
+	text_to_send = "A spot is available in the " + county + " county! Click " + page + " here to reserve."
 	
 	#Open the webpage using Firefox
 	driver = webdriver.Firefox()
 	driver.get(page)
+	#Need to wait a bit for the redirect
+	time.sleep(2)
 	htmlcode = driver.page_source
-	#We search in the "69" county, if there is at least 1 available spot (1 to 99)
-	x = re.findall('69 ('+'([1-9]|[1-8][0-9]|9[0-9])' + 'places disponibles', htmlcode)
-			
-	#Always true, unless we reach the "break"
-	while True: #Always true, unless we reach the "break"
-		#If there is an available place, the x string should not be empty send the text message and exit the program
-		if not x:			
-			os.system("osascript imessage.scpt %s '%s' " % (tel, text_to_send))
+	
+	place_available = search_places_text(county,htmlcode)
+
+	#Always true, unless we reach the "break"	
+	while True:
+		#If there is an available place, the place_available should be "True" and we should send the text message.
+		print(place_available)
+		if place_available == False:		
+			os.system("osascript imessage.scpt %s '%s' " % (phone, text_to_send))
 			break
 		#If there is no place available, refresh the page (after a while) and repeat.
-		time.sleep(int(refresh_rate_seconds))
-		driver.refresh()
+		time.sleep(int(refresh_time_seconds))
+		driver.refresh()	
+
+
+def main ():
+	parser = argparse.ArgumentParser(description='A simple script to find driving test spots.')
+	parser.add_argument('phone', nargs='?', default="+11111111111", type=str, help='Phone number to send the notification to.')
+	parser.add_argument('refresh_time_seconds', nargs='?', default=3, type=int, help='Time to wait before refreshing the pahe (in seconds)')
+	parser.add_argument('county', nargs='?', default=69, type=str, help='County number to scan')
+	options = parser.parse_args()
+
+	phone = options.phone
+	refresh_time_seconds = options.refresh_time_seconds
+	county = str(options.county)
+	
+	#Define the different variables
+	page = "https://XXX.com"
+
+	scan_page(page,phone,refresh_time_seconds,county)
+
 			
-main ()
+if __name__ == '__main__':
+    main()
